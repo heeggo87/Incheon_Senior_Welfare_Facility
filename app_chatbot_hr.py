57101a728f079f6b4a1ca690f2cbe30a95f8b210
heeggo87 <heeggo87@gmail.com>
2025-12-29 18:05:42 +0900

    건강보험공단csv파일 경로변경

--- app_chatbot_hr.py
+++ app_chatbot_hr.py
@@ -7,10 +7,18 @@ import PyPDF2
 import re
 import os
 from pypdf import PdfReader
+from pathlib import Path
 
 # 데이터 파일 불러오기
 health_institutions = pd.read_csv('./data/인천광역시_건강검진기관.csv', encoding='cp949', sep='\t')
-health_check_data = pd.read_csv('./data/국민건강보험공단_건강검진정보_2024.csv', encoding="cp949")
+# health_check_data = pd.read_csv('./data/국민건강보험공단_건강검진정보_2024.csv', encoding="cp949")
+
+BASE_DIR = Path(__file__).resolve().parent
+data_path = BASE_DIR / "data" / "국민건강보험공단_건강검진정보_2024.csv"
+
+health_check_data = pd.read_csv(data_path, encoding="cp949")
+
+
 
 # --- RAG(CHROMA) 통합: app_testchatbot의 캐시된 벡터스토어/체인을 사용 ---
 # app_testchatbot.py에 정의된 load_vectorstore, make_rag_chain를 재사용합니다.
